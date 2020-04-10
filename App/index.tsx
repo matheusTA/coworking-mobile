93ae1d8a9b10a2eb77e2662c52c30a9f3b931665
Matheus Antonino <matrajano12@gmail.com>
2020-04-10 19:28:36 -0300

    Update config splash screen adn icon

--- App/index.tsx
+++ App/index.tsx
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
 });
