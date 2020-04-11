46271d611623fda36d333778aaf4faeb8ae531dd
Matheus Antonino <matrajano12@gmail.com>
2020-04-11 01:48:19 -0300

    Update component app

--- App/index.tsx
+++ App/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 
-export default function App() {
+import SignIn from "./pages/SignIn";
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Coworking mobile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SignIn />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
+
+export default App;
