dfea2b37cf70f24d94c359680e5cf84a7cd715e0
Matheus Antonino <matrajano12@gmail.com>
2020-04-12 22:32:22 -0300

    Custimize header

--- App/routes/map.routes.tsx
+++ App/routes/map.routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import normalize from "react-native-normalize";
 
 import MapPage from "../pages/Map";
 import CompanyProfilePage from "../pages/CompanyProfile";
@@ -9,8 +10,48 @@ const Stack = createStackNavigator();
 const MapRoutes: React.FC = () => {
   return (
     <Stack.Navigator initialRouteName="MapPage">
-      <Stack.Screen name="MapPage" component={MapPage} />
-      <Stack.Screen name="CompanyProfilePage" component={CompanyProfilePage} />
+      <Stack.Screen
+        name="MapPage"
+        component={MapPage}
+        options={{
+          headerTitle: "Coworking",
+          headerTitleStyle: {
+            color: "#F95F62",
+            fontWeight: "bold",
+            fontSize: normalize(30),
+          },
+          headerTitleAlign: "center",
+          // headerRight: () => (
+          //   <Button
+          //     onPress={() => alert("This is a button!")}
+          //     title="Info"
+          //     color="#000"
+          //   />
+          // ),
+        }}
+      />
+      <Stack.Screen
+        name="CompanyProfilePage"
+        component={CompanyProfilePage}
+        options={{
+          headerTitle: "Coworking",
+          headerTitleStyle: {
+            color: "#F95F62",
+            fontWeight: "bold",
+            fontSize: normalize(30),
+          },
+          headerTitleAlign: "center",
+          headerBackTitle: "Voltar",
+          headerBackTitleVisible: true,
+          // headerRight: () => (
+          //   <Button
+          //     onPress={() => alert("This is a button!")}
+          //     title="Info"
+          //     color="#000"
+          //   />
+          // ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
--- App/routes/search.routes.tsx
+++ App/routes/search.routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import normalize from "react-native-normalize";
 
 import SearchPage from "../pages/Search";
 import CompanyProfilePage from "../pages/CompanyProfile";
@@ -9,8 +10,48 @@ const Stack = createStackNavigator();
 const SearchRoutes: React.FC = () => {
   return (
     <Stack.Navigator initialRouteName="SearchPage">
-      <Stack.Screen name="SearchPage" component={SearchPage} />
-      <Stack.Screen name="CompanyProfilePage" component={CompanyProfilePage} />
+      <Stack.Screen
+        name="SearchPage"
+        component={SearchPage}
+        options={{
+          headerTitle: "Coworking",
+          headerTitleStyle: {
+            color: "#F95F62",
+            fontWeight: "bold",
+            fontSize: normalize(30),
+          },
+          headerTitleAlign: "center",
+          // headerRight: () => (
+          //   <Button
+          //     onPress={() => alert("This is a button!")}
+          //     title="Info"
+          //     color="#000"
+          //   />
+          // ),
+        }}
+      />
+      <Stack.Screen
+        name="CompanyProfilePage"
+        component={CompanyProfilePage}
+        options={{
+          headerTitle: "Coworking",
+          headerTitleStyle: {
+            color: "#F95F62",
+            fontWeight: "bold",
+            fontSize: normalize(30),
+          },
+          headerTitleAlign: "center",
+          headerBackTitle: "Voltar",
+          headerBackTitleVisible: true,
+          // headerRight: () => (
+          //   <Button
+          //     onPress={() => alert("This is a button!")}
+          //     title="Info"
+          //     color="#000"
+          //   />
+          // ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
