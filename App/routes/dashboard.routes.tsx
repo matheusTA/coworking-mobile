5b351936e50f3749480999d62876293601ea04e0
Matheus Antonino <matrajano12@gmail.com>
2020-04-13 01:08:13 -0300

    Add button tab

--- App/routes/dashboard.routes.tsx
+++ App/routes/dashboard.routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import MapRoutes from "./map.routes";
 import SearchRoutes from "./search.routes";
@@ -8,9 +9,31 @@ const Tab = createBottomTabNavigator();
 
 const DashboardRoutes: React.FC = () => {
   return (
-    <Tab.Navigator initialRouteName="MapRoutes">
-      <Tab.Screen name="MapRoutes" component={MapRoutes} />
-      <Tab.Screen name="SearchRoutes" component={SearchRoutes} />
+    <Tab.Navigator
+      initialRouteName="MapRoutes"
+      tabBarOptions={{ activeTintColor: "#F95F62" }}
+    >
+      <Tab.Screen
+        name="MapRoutes"
+        component={MapRoutes}
+        options={{
+          tabBarLabel: "Mapa",
+          tabBarIcon: ({ color, focused, size }) => {
+            const iconName = `map-marked${focused ? "" : "-alt"}`;
+            return <FontAwesome5 name={iconName} color={color} size={size} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="SearchRoutes"
+        component={SearchRoutes}
+        options={{
+          tabBarLabel: "Pesquisar",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="search-location" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
