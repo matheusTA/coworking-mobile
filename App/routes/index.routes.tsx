c745c554e145dc2742c0098f8b0441c4288c1ba8
Matheus Antonino <matrajano12@gmail.com>
2020-04-23 00:28:34 -0300

    Rendering route with context auth

--- App/routes/index.routes.tsx
+++ App/routes/index.routes.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import { useAuth } from "../contexts/auth";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import DashboardRoutes from "./dashboard.routes";
 
 const Stack = createStackNavigator();
 
+const AuthRoutes: React.FC = () => (
+  <Stack.Navigator initialRouteName="DashboardRoutes" headerMode="none">
+    <Stack.Screen name="DashboardRoutes" component={DashboardRoutes} />
+  </Stack.Navigator>
+);
+
+const AppRoutes: React.FC = () => (
+  <Stack.Navigator initialRouteName="SignIn" headerMode="none">
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
+);
+
 const Routes: React.FC = () => {
-  return (
-    <Stack.Navigator initialRouteName="SignIn" headerMode="none">
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
-      <Stack.Screen name="DashboardRoutes" component={DashboardRoutes} />
-    </Stack.Navigator>
-  );
+  const { signed, loading } = useAuth();
+
+  return signed ? <AuthRoutes /> : <AppRoutes />;
 };
 
 export default Routes;
