9e970e4bf1e961a0ddcee703a367856cf8e404b7
Matheus Antonino <matrajano12@gmail.com>
2020-06-07 17:22:01 -0300

    Add socket io

--- App/index.tsx
+++ App/index.tsx
@@ -1,11 +1,38 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
+import socketio from "socket.io-client";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import { AuthProvider } from "./contexts/auth";
 import Routes from "./routes/index.routes";
+import { Alert, AsyncStorage } from "react-native";
+import { BASE_URL } from "./config/environment";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    AsyncStorage.getItem("@Auth:user").then((userJson) => {
+      if (userJson) {
+        const { _id } = JSON.parse(userJson);
+        const socket = socketio(BASE_URL, {
+          query: { company_id: _id },
+        });
+
+        socket.on("booking_response", (booking: any) => {
+          Alert.alert(
+            `Sua reserva em ${booking.spot.name} no ${format(
+              new Date(booking.date),
+              "'Dia' dd 'de' MMMM'",
+              {
+                locale: pt,
+              }
+            )} foi ${booking.approved ? "APROVADA" : "REJEITADA"}`
+          );
+        });
+      }
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <AuthProvider>
